34e1b42468608a91c30cfa8908374e3a033dd59e
Bennett Johnson <bennettjohnson0@gmail.com>
2025-09-03 20:01:50 -0500

    icons and other stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg' ],
+      manifest: {
+        name: 'Art Slideshow',
+        short_name: 'ArtSlideshow',
+        description: 'A slideshow which connects to a remote server to display images randomly at configurable intervals',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'fullscreen',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }),
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
-  }
+  },
 })
